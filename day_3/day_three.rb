1492ba58d96eac2da77fb6b8b1f5631a20fd5aae
Corwin Brown <blakfeld@nightwing.lan>
2022-12-03 21:49:45 -0600

    day 3

---
+++ day_3/day_three.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+SCORES = [*('a'..'z'), *('A'..'Z')].map.with_index(1) { |char, i| [char, i] }.to_h
+
+def part_one(rucksack_contents)
+  rucksack_contents
+    .map { |line| line.chars.each_slice(line.size / 2).to_a }
+    .flat_map { |a, b| a.intersection(b) }
+    .map { |c| SCORES[c] }
+    .sum
+end
+
+def part_two(rucksack_contents)
+  rucksack_contents
+    .map(&:chars)
+    .each_slice(3)
+    .flat_map { |chunk| chunk.inject(:&) }
+    .map { |char| SCORES[char] }
+    .sum
+end
+
+
+def main
+  input_path = ARGV[0]
+  rucksack_contents = File.readlines(input_path).map(&:chomp)
+
+  part_one_ans = part_one(rucksack_contents)
+  puts "Part 1: #{part_one_ans}"
+
+  part_two_ans = part_two(rucksack_contents)
+  puts "Part 2: #{part_two_ans}"
+end
+
+main
